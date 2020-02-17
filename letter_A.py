8be856f9804718c038d19f211f1985e106887870
RofiatIbrahim <60641986+RofiatIbrahim@users.noreply.github.com>
2020-02-17 11:02:51 +0100

    Add files via upload

---
+++ letter_A.py
@@ -0,0 +1,19 @@
+
+a = input("should the letter A be small or big :")
+if a=="big":
+ print("      ***")
+ print("     *   *")
+ print("    *     *")
+ print("   *********")
+ print("  *         *")
+ print(" *           *")
+ print("*             *")
+elif a=="small":
+ print("    *")
+ print("   * *")
+ print("  *****")
+ print(" *     *")
+ print("*       *")
+else:
+ print("come back when it has being upgraded")
+    
